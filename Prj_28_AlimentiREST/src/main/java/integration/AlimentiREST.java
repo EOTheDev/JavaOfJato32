18d6ee65d92cc170907b04743b03271a33219497
EOTheDev <edo17maggio@gmail.com>
2021-07-03 10:10:38 +0200

    update

--- Prj_28_AlimentiREST/src/main/java/integration/AlimentiREST.java
+++ Prj_28_AlimentiREST/src/main/java/integration/AlimentiREST.java
@@ -1,11 +1,20 @@
 package integration;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entities.Alimento;
+import service.AlimentoService;
+import service.AlimentoServiceImpl;
 
 /**
  * Servlet implementation class AlimentiREST
@@ -13,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/api/alimenti")
 public class AlimentiREST extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	AlimentoService service;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -20,7 +30,7 @@ public class AlimentiREST extends HttpServlet {
     public AlimentiREST() {
         super();
         System.out.println("Servlet costruita");
-    
+        this.service= new AlimentoServiceImpl();
     }
 
 	/**
@@ -29,7 +39,18 @@ public class AlimentiREST extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("hai chiamato via GET");
 		System.out.println("vuoi ricevere tutti gli alimenti");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<Alimento> alimenti= this.service.findAll();
+		JSONArray json= new JSONArray();
+		for (Alimento a : alimenti) {
+			JSONObject obj = new JSONObject();
+			obj.put("alimento", a);
+			
+		}
+		
+		
+		response.getWriter().append(json.toString());
+		
 	}
 
 	/**
--- Prj_28_AlimentiREST/src/main/java/integration/CategorieREST.java
+++ Prj_28_AlimentiREST/src/main/java/integration/CategorieREST.java
@@ -1,11 +1,20 @@
 package integration;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entities.Alimento;
+import service.AlimentoService;
+import service.AlimentoServiceImpl;
 
 /**
  * Servlet implementation class CategorieREST
@@ -13,13 +22,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/api/categorie")
 public class CategorieREST extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	AlimentoService service;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public CategorieREST() {
         super();
         System.out.println("Servlet costruita");
+        this.service= new AlimentoServiceImpl();
+
     }
 
 	/**
@@ -28,7 +40,18 @@ public class CategorieREST extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("hai chiamato via GET");
 		System.out.println("vuoi ricevere tutti le categorie");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String cat="formaggi"
+;		List<Alimento> categorie= this.service.findByCat(cat);
+		JSONArray json= new JSONArray();
+		for (String cate : categorie) {
+			JSONObject obj = new JSONObject();
+			obj.put("categoria", cate);
+			
+		}
+		
+		
+		response.getWriter().append(json.toString());
+		
 	}
 
 	/**
